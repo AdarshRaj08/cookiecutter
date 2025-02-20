0111721439298fe53f76603a64bea1368d09fed2
Adarsh Raj <iamadarsh008@gmail.com>
2025-02-20 12:38:28 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='project in which using first time cookiecutter',
+    author='adarsh',
+    license='',
+)
